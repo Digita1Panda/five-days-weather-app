8bf1a760b0c42cd478ab5c67f7d815dc806619f4
Digita1Panda <prodigykyo@gmail.com>
2024-01-28 08:39:46 +0000

    functions to fetch geocoordinates and data

--- script.js
+++ script.js
@@ -3,9 +3,36 @@ const cityInput = document.querySelector(".city-input");
 
 searchBtn.addEventListener("click", getCityCoordinates);
 
+const apiKey = "e1ab8ca6e0505dab1bbf3ada39e01168";
+
 function getCityCoordinates() {
   const cityName = cityInput.value.trim();
   if (!cityName) return;
 
-  console.log(cityName);
+  const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
+
+  fetch(queryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (!data.length)
+        return alert(
+          `${cityName} not found. Please input a correct city name.`
+        );
+      const { name, lat, lon } = data[0];
+      getWeatherDetails(name, lat, lon);
+    });
+}
+
+function getWeatherDetails(cityName, lat, lon) {
+  const weatherApiURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  fetch(weatherApiURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
