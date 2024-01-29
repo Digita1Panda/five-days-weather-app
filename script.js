6acadca8aed5b2aa44f1edc57cab38a8d65ef98f
Digita1Panda <prodigykyo@gmail.com>
2024-01-29 02:42:28 +0000

    made sure code is readable

--- script.js
+++ script.js
@@ -1,38 +1,105 @@
-const searchBtn = document.querySelector(".search-btn");
+const searchButton = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".city-input");
-
-searchBtn.addEventListener("click", getCityCoordinates);
+const weatherCardsDiv = document.querySelector(".weather-cards");
+const todaysWeather = document.querySelector(".current-weather");
 
 const apiKey = "e1ab8ca6e0505dab1bbf3ada39e01168";
 
-function getCityCoordinates() {
-  const cityName = cityInput.value.trim();
-  if (!cityName) return;
+function weatherCards(cityName, forecastItem, index) {
+  if (index === 0) {
+    return `<div class="details">
+            <h2>${cityName} (${forecastItem.dt_txt.split(" ")[0]})</h2>
+            <h4>Temperature: ${(forecastItem.main.temp - 273.15).toFixed(
+              1
+            )}°C</h4>
+            <h4>Wind: ${forecastItem.wind.speed} KPH</h4>
+            <h4>Humidity: ${forecastItem.main.humidity}%</h4>
+        </div>
+        <div class="icon">
+            <img
+              src="https://openweathermap.org/img/wn/${
+                forecastItem.weather[0].icon
+              }@4x.png"
+              alt="weather-icon"
+            />
+            <h4>${forecastItem.weather[0].description}</h4>
+          </div>`;
+  } else {
+    return `<li class="card">
+                      <h3>(${forecastItem.dt_txt.split(" ")[0]})</h3>
+                      <img
+                      src="https://openweathermap.org/img/wn/${
+                        forecastItem.weather[0].icon
+                      }@2x.png"
+                      alt="weather-icon"
+                      />
+                      <h4>Temp: ${(forecastItem.main.temp - 273.15).toFixed(
+                        1
+                      )}°C</h4>
+                      <h4>Wind: ${forecastItem.wind.speed} KPH</h4>
+                      <h4>Humidity: ${forecastItem.main.humidity}%</h4>
+                  </li>`;
+  }
+}
 
-  const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
+function weatherDetails(cityName, lat, lon) {
+  const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+  console.log(weatherURL);
+
+  fetch(weatherURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const uniqueDays = [];
+      console.log(data);
+
+      const nextFiveDays = data.list.filter((forecast) => {
+        const eachDate = new Date(forecast.dt_txt).getDate();
+        if (!uniqueDays.includes(eachDate)) {
+          return uniqueDays.push(eachDate);
+        }
+      });
+
+      cityInput.value = "";
+      weatherCardsDiv.innerHTML = "";
+      todaysWeather.innerHTML = "";
+
+      console.log(nextFiveDays);
+      nextFiveDays.forEach((forecastItem, index) => {
+        if (index === 0) {
+          todaysWeather.insertAdjacentHTML(
+            "beforeend",
+            weatherCards(cityName, forecastItem, index)
+          );
+        } else {
+          weatherCardsDiv.insertAdjacentHTML(
+            "beforeend",
+            weatherCards(cityName, forecastItem, index)
+          );
+        }
+      });
+    });
+}
+
+function cityCoordinates() {
+  const cityName = cityInput.value.trim();
+  console.log(cityName);
+
+  const queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
+
+  console.log(queryURL);
 
   fetch(queryURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      if (!data.length)
-        return alert(
-          `${cityName} not found. Please input a correct city name.`
-        );
-      const { name, lat, lon } = data[0];
-      getWeatherDetails(name, lat, lon);
-    });
-}
-
-function getWeatherDetails(cityName, lat, lon) {
-  const weatherApiURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-  fetch(weatherApiURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
       console.log(data);
+      const { name, lat, lon } = data[0];
+      weatherDetails(name, lat, lon);
     });
 }
+
+searchButton.addEventListener("click", cityCoordinates);
