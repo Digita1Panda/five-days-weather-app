1c76a15851897312ecdab73668ed4df922c4255b
Digita1Panda <prodigykyo@gmail.com>
2024-01-30 21:53:56 +0000

    added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Declaring global variables
 const searchButton = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".city-input");
 const weatherCardsDiv = document.querySelector(".weather-cards");
@@ -6,6 +7,7 @@ const searchHistory = document.querySelector(".stored-locations");
 
 const apiKey = "e1ab8ca6e0505dab1bbf3ada39e01168";
 
+// Function for the weather forecast cards for current and the next five days.
 function weatherCards(cityName, forecastItem, index) {
   if (index === 0) {
     return `<div class="details">
@@ -43,9 +45,11 @@ function weatherCards(cityName, forecastItem, index) {
   }
 }
 
+// Using the parameters gets the lat and lon Geo-coordinates and inserts it.
 function weatherDetails(cityName, lat, lon) {
   const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
+  // using the fetch to get the data from API
   fetch(weatherURL)
     .then(function (response) {
       return response.json();
@@ -60,6 +64,7 @@ function weatherDetails(cityName, lat, lon) {
         }
       });
 
+      // Inserting it into the HTML using a callback of weatherCards function
       cityInput.value = "";
       weatherCardsDiv.innerHTML = "";
       todaysWeather.innerHTML = "";
@@ -80,9 +85,11 @@ function weatherDetails(cityName, lat, lon) {
     });
 }
 
+// Grabbing the city coordinates
 function cityCoordinates() {
   const cityName = cityInput.value.trim();
 
+  // uses the API key to fetch data
   const queryURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
 
   fetch(queryURL)
@@ -95,6 +102,7 @@ function cityCoordinates() {
     });
 }
 
+// function for local storage and creation of the button on reload and on click it will load those data
 function loadSearchHistoryFromLocalStorage() {
   let searches = JSON.parse(localStorage.getItem("searches")) || [];
   searchHistory.innerHTML = "";
@@ -109,12 +117,14 @@ function loadSearchHistoryFromLocalStorage() {
   });
 }
 
+// saving the searches in local storage
 function saveSearchToLocalStorage(cityName) {
   let searches = JSON.parse(localStorage.getItem("searches")) || [];
   searches.push(cityName);
   localStorage.setItem("searches", JSON.stringify(searches));
 }
 
+// function defining what the will happen upon click. Calling the save storage function with the argument of the city name
 searchButton.addEventListener("click", function () {
   const capitalizedCityName =
     cityInput.value.charAt(0).toUpperCase() + cityInput.value.slice(1);
@@ -124,6 +134,7 @@ searchButton.addEventListener("click", function () {
   cityCoordinates();
 });
 
+// Ensure it will do the same also upon Enter key
 cityInput.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     const capitalizedCityName =
@@ -134,4 +145,5 @@ cityInput.addEventListener("keydown", function (event) {
   }
 });
 
+// Calling the load storage function
 loadSearchHistoryFromLocalStorage();
